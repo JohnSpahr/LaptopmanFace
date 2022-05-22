12eaaf9343f1c063ea202d9163c9c47a771ea4f1
JohnS <37165173+JohnSpahr@users.noreply.github.com>
2022-05-22 17:45:00 -0400

    Version 1.4
    Pebble Time Round support is finally here!

--- README.md
+++ README.md
@@ -8,11 +8,7 @@ A watch face for Pebble watches starring my signature "Laptopman" avatar. Shake 
 ![Color](screenshots/color_evil.png)
 
 ## Supported Devices
-- OG Pebble
-- Pebble Steel
-- Pebble Time
-- Pebble Time Steel
-- Pebble 2
+- As of v1.4, all of the Pebbles!
 
 ## Check It Out!
 Want to try it out? If so, you can get the PBW from the [releases page](https://github.com/johnspahr/laptopmanface/releases) or from [Rebble](https://apps.rebble.io/en_US/application/626b1ef27ca61400094ed7e4).
--- app/src/main.c
+++ app/src/main.c
@@ -3,7 +3,7 @@
 
   Created by John Spahr (johnspahr.org)
 
-  Thanks to: "PebbleFaces" example, the watch face creation guide hosted by Rebble, the Rebble team, and all of you for using my first watch face!
+  Thanks to: "PebbleFaces" example, the watch face creation guide hosted by Rebble, the Rebble team, Robert Smith, and all of you for using my first watch face!
 
   One more thing: The Laptopman is my own work of art, perhaps even my magnum opus. It's pretty great.
 */
@@ -12,11 +12,13 @@
 static Window *window;            // window object
 static TextLayer *s_time_layer;   // the clock (text layer)
 static BitmapLayer *bitmap_layer; // layer for display the bitmap
-static GBitmap *current_bmp;      // bitmap object
+static GBitmap *current_bmp;      // current bitmap object
 static GBitmap *mono_laptopman;   // mono laptopman bitmap
 static GBitmap *color_laptopman;  // color laptopman bitmap
+static GBitmap *round_laptopman;  // round laptopman bitmap
 static GBitmap *mono_angry;       // mono angry laptopman bitmap
 static GBitmap *color_angry;      // color angry laptopman bitmap
+static GBitmap *round_angry;      // round angry laptopman bitmap
 
 static void update_time()
 {
@@ -53,7 +55,8 @@ static void show_laptopman()
     current_bmp = mono_laptopman;                          // update current bitmap object
     break;
   case PlatformTypeBasalt:
-    // color pebbles
+    // color (rectangular) pebbles
+    bitmap_layer_set_background_color(bitmap_layer, GColorLiberty); // set blue background color for bitmap layer
     if (!color_laptopman)
     {
       color_laptopman = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_COLOR_LAPTOPMAN); // create bitmap from image resource if it doesn't exist yet
@@ -61,19 +64,30 @@ static void show_laptopman()
     bitmap_layer_set_bitmap(bitmap_layer, color_laptopman); // set bitmap layer image
     current_bmp = color_laptopman;                          // update current bitmap object
     break;
+  case PlatformTypeChalk:
+    // pebble time round
+    bitmap_layer_set_background_color(bitmap_layer, GColorLiberty); // set blue background color for bitmap layer
+    if (!round_laptopman)
+    {
+      round_laptopman = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ROUND_LAPTOPMAN); // create bitmap from image resource if it doesn't exist yet
+    }
+    bitmap_layer_set_bitmap(bitmap_layer, round_laptopman); // set bitmap layer image
+    current_bmp = round_laptopman;                          // update current bitmap object
+    break;
   }
 }
 
-void tap_handler(AccelAxisType accel, int32_t direction)
+static void bluetooth_callback(bool connected)
 {
-  // when watch is shaken, determine which bitmap is showing
-  if (current_bmp != mono_laptopman && current_bmp != color_laptopman)
+  if (connected)
   {
-    show_laptopman(); // show default laptopman bitmap if angry bitmap is currently showing
+    show_laptopman(); // show nice laptopman if connected
   }
   else
   {
     // otherwise, show angry laptopman bitmap
+    vibes_double_pulse(); // issue a vibrating alert on disconnect
+
     switch (PBL_PLATFORM_TYPE_CURRENT)
     {
     // detect platform...
@@ -88,13 +102,24 @@ void tap_handler(AccelAxisType accel, int32_t direction)
       current_bmp = mono_angry;                          // update current bitmap object
       break;
     case PlatformTypeBasalt:
-      // color pebbles
+      // color (rectangular) pebbles
+      bitmap_layer_set_background_color(bitmap_layer, GColorDarkCandyAppleRed); // set red background color for bitmap layer
       if (!color_angry)
       {
         color_angry = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_COLOR_ANGRY); // create bitmap from image resource if it doesn't exist yet
       }
       bitmap_layer_set_bitmap(bitmap_layer, color_angry); // set bitmap layer image
       current_bmp = color_angry;                          // update current bitmap object
+      break;
+    case PlatformTypeChalk:
+      // pebble time round
+      bitmap_layer_set_background_color(bitmap_layer, GColorDarkCandyAppleRed); // set red background color for bitmap layer
+      if (!round_angry)
+      {
+        round_angry = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ROUND_ANGRY); // create bitmap from image resource if it doesn't exist yet
+      }
+      bitmap_layer_set_bitmap(bitmap_layer, round_angry); // set bitmap layer image
+      current_bmp = round_angry;                          // update current bitmap object
       break;
     }
   }
@@ -107,14 +132,13 @@ static void window_load(Window *window)
 
   bitmap_layer = bitmap_layer_create(bounds);                          // create bitmap layer
   layer_add_child(window_layer, bitmap_layer_get_layer(bitmap_layer)); // add bitmap layer to window
-  current_bmp = NULL;                                                  // set bitmap object to null
 
   // create clock text layer...
   s_time_layer = text_layer_create(
-      GRect(0, 25, bounds.size.w, bounds.size.h));
+      GRect(PBL_IF_COLOR_ELSE(3, 0), PBL_IF_ROUND_ELSE(26, 24), bounds.size.w, bounds.size.h));
 
   // time text layer setup...
-  text_layer_set_background_color(s_time_layer, GColorClear);                        // clear background color
+  text_layer_set_background_color(s_time_layer, GColorClear);                        // set clear background color
   text_layer_set_text_color(s_time_layer, GColorWhite);                              // white text color
   text_layer_set_text(s_time_layer, "00:00");                                        // set text to 00:00
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD)); // set font to Gothic, 28pt, bold
@@ -157,6 +181,7 @@ static void init(void)
   update_time(); // register with TickTimerService
 
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler); // set tick_handler function as time handler (start keeping time, essentially. kinda important on a watch.)
+
 }
 
 static void deinit(void)
